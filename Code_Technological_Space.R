046c0d720cae73d5460c72205720534fd3530432
matheusleusin <58182885+matheusleusin@users.noreply.github.com>
2020-06-19 19:25:45 +0200

    Commit7

--- Code_Technological_Space.R
+++ Code_Technological_Space.R
@@ -12,7 +12,9 @@ library(ggplot2)
 library("data.table") #for reading the big files using fread and for replacing countries names (by AI_pat for example)
 
 #1.NACE data -----
-#1.1. First part -----
+#1.1. Sparse matrix -----
+#On this first part we will create the sparse matrix, calculate the similarity matrix and save it in a csv file 
+#named "Matrix_Nace"
 rm(list=ls())
 #setwd("C:/Users/mathe/OneDrive/?rea de Trabalho")
 setwd("C:/Users/Matheus/Desktop") #for loading the big file
@@ -50,7 +52,6 @@ create_sparse_matrix <- function(i.input, j.input){
   colnames(mat) <- j.input %>% factor() %>% levels()
   return(mat)
 }
-
 
 mat_tech_AI1 <- create_sparse_matrix(i = Nace_all_patents_Part1 %>% pull(appln_id),
                                     j = Nace_all_patents_Part1 %>% pull(nace2_code))
@@ -151,48 +152,8 @@ rm(Nace_all_patents_Part1)
 rm(Nace_all_patents_Part2)
 rm(Nace_all_patents_Part3)
 
-library(janitor)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-#matrix2 <- read.csv.ffdf(file="Data/Matrix_Nace.csv", header = F, sep = ";")
-
-matrix2 <- read.csv("Data/Matrix_Nace.csv", sep = ";", header = F)
-matrix2 <- matrix2 %>%
-  row_to_names(row_number = 1)
-matrix <- matrix2[,-1]
-rownames(matrix) <- matrix2[,1]
-matrix <- as.matrix(matrix)
-mat_tech_AI_Final <- matrix
-
-#ALL RIGHT UNTIL HERE
-mat_tech_rel_AI <- mat_tech_AI_Final %>% 
-  relatedness(method = "cosine")
-
-nace2_names <- read.csv("Data/tls902_ipc_nace2.csv", sep = ";", header = TRUE)%>%
-  select(nace2_code, nace2_descr) %>%
-  distinct(nace2_code, .keep_all = TRUE) %>%
-  mutate(nace2_code = nace2_code) %>%
-  arrange(nace2_code)
-
-g_tech_AI <- mat_tech_rel_AI %>% as_tbl_graph(directed = FALSE) %N>%
-  left_join(nace2_names %>% mutate(nace2_code = nace2_code %>% as.character()), by = c("name" = "nace2_code")) %>%
-  mutate(dgr = centrality_eigen(weights = weight)) %E>%
-  filter(weight >= mean(weight))
-
-coords_tech_AI <- g_tech_AI %>% igraph::layout.fruchterman.reingold() %>% as_tibble()
-colnames(coords_tech_AI) <- c("x", "y")
-
-#let's take a look at the most and less complex Nace fields:
-g_tech_AI %N>% 
-  arrange(desc(dgr)) %>%
-  as_tibble() %>%
-  slice(1:10)
-
-g_tech_AI %N>% 
-  arrange(dgr) %>%
-  as_tibble() %>%
-  slice(1:10)
-
-#1.2.Nace By Country AI----
+#1.2.Technology Space ----
+#Now we will create the technology spaces, dividem them in 3 periods;
 setwd("C:/Users/Matheus/Desktop") 
 #rm(list=ls())
 
@@ -211,7 +172,8 @@ group_by_ctry_and_nace <- function (data){
     drop_na() 
 }
 
-#First part
+#1.2.1.First Period ----
+#For the first period, we need only the dataset from Part2:
 setwd("C:/Users/Matheus/Desktop") 
 c <- 58935336-40000000
 Nace_all_patents_Part1 <- fread("All_patents_and_Naces_Part1.csv", header = F, nrow = 20000000)
@@ -299,125 +261,9 @@ names(tabledatafinal) <- c("ctry_code", "nace2_code", "n_tech_reg")
 
 write.csv2(tabledatafinal, file = "Data/Data_reg_tech.csv", row.names = F)
 
-
-#for loading the big file
-
-
-c <- 58935336-40000000
-Nace_all_patents_Part1 <- fread("All_patents_and_Naces_Part1.csv", header = F, nrow = 20000000)
-names(Nace_all_patents_Part1) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-Nace_all_patents_Part1 <- Nace_all_patents_Part1[, c((-4), (-5))]
-
-reg_tech1 <- Nace_all_patents_Part1 %>%
-  group_by(appln_id) %>%
-  mutate(field_weight = 1 / n()) %>%
-  ungroup()
-
-rm(Nace_all_patents_Part1)
-
-Nace_all_patents_Part2 <- fread("All_patents_and_Naces_Part1.csv", header = F, nrow = 20000000, skip = 20000000)
-names(Nace_all_patents_Part2) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-Nace_all_patents_Part2 <- Nace_all_patents_Part2[, c((-4), (-5))]
-
-reg_tech2 <- Nace_all_patents_Part2 %>%
-  group_by(appln_id) %>%
-  mutate(field_weight = 1 / n()) %>%
-  ungroup()
-
-rm(Nace_all_patents_Part2)
-
-Nace_all_patents_Part3 <- fread("All_patents_and_Naces_Part1.csv", header = F, nrow = c, skip = 40000000)
-names(Nace_all_patents_Part3) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-Nace_all_patents_Part3 <- Nace_all_patents_Part3[, c((-4), (-5))]
-
-reg_tech3 <- Nace_all_patents_Part3 %>%
-  group_by(appln_id) %>%
-  mutate(field_weight = 1 / n()) %>%
-  ungroup()
-
-rm(Nace_all_patents_Part3)
-reg_tech_temp <- rbind(reg_tech1, reg_tech2)
-rm(reg_tech1)
-rm(reg_tech2)
-reg_tech_temp <- rbind(reg_tech_temp, reg_tech3)
-rm(reg_tech3)
-
-reg_tech_temp2 <- reg_tech_temp
-
-reg_tech_temp2 %<>%
-  group_by(ctry_code, nace2_code) %>%
-  summarise(n_tech_reg = sum(field_weight)) %>%
-  ungroup() %>%
-  drop_na() 
-
-#Second Part:
-c <- 45233329 -40000000
-Nace_all_patents_Part4 <- fread("All_patents_and_Naces_Part2.csv", header = F, nrow = 20000000)
-names(Nace_all_patents_Part4) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-Nace_all_patents_Part4 <- Nace_all_patents_Part4[, c((-4), (-5))]
-
-reg_tech4 <- Nace_all_patents_Part4 %>%
-  group_by(appln_id) %>%
-  mutate(field_weight = 1 / n()) %>%
-  ungroup()
-
-rm(Nace_all_patents_Part4)
-
-
-Nace_all_patents_Part5 <- fread("All_patents_and_Naces_Part2.csv", header = F, nrow = 20000000, skip = 20000000)
-names(Nace_all_patents_Part5) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-Nace_all_patents_Part5 <- Nace_all_patents_Part5[, c((-4), (-5))]
-
-reg_tech5 <- Nace_all_patents_Part5 %>%
-  group_by(appln_id) %>%
-  mutate(field_weight = 1 / n()) %>%
-  ungroup()
-
-rm(Nace_all_patents_Part5)
-
-Nace_all_patents_Part6 <- fread("All_patents_and_Naces_Part2.csv", header = F, nrow = c, skip = 40000000)
-names(Nace_all_patents_Part6) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-Nace_all_patents_Part6 <- Nace_all_patents_Part6[, c((-4), (-5))]
-
-reg_tech6 <- Nace_all_patents_Part6 %>%
-  group_by(appln_id) %>%
-  mutate(field_weight = 1 / n()) %>%
-  ungroup()
-
-rm(Nace_all_patents_Part6)
-
-#Nace_all_patents_Part2 <- fread("All_patents_and_Naces_Part1.csv", header = F, nrow = 20000000, skip = 20000000)
-#Nace_all_patents_Part3 <- fread("All_patents_and_Naces_Part1.csv", header = F, nrow = c, skip = 40000000)
-
-#names(Nace_all_patents_Part1) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-#Nace_all_patents_Part1 <- Nace_all_patents_Part1[, c((-4), (-5))]
-
-#Nace_all_patents_Part2 <- fread("All_patents_and_Naces_Part2.csv", header = F)
-#names(Nace_all_patents_Part2) <- c("appln_id", "ctry_code", "nace2_code", "weight", "priority_year")
-#Nace_all_patents_Part2 <- Nace_all_patents_Part2[, c((-4), (-5))]
-
-#Nace_all_patents <- rbind(Nace_all_patents_Part1, Nace_all_patents_Part2)
-
-#rm(Nace_all_patents_Part1)
-#rm(Nace_all_patents_Part2)
-
-#Nace_all_patents_applnId <- Nace_all_patents[, (1)]
-
-
-#here commes the first big problem: I don't have the Nace_all_patents file anymore, which I need for calculating the weights;
-#maybe I can load both files and merge by rows (rbind)
-
-#I'll do the one below at the end:
-countries_geo <- Nace_all_patents %>%
-  distinct(ctry_code, .keep_all = TRUE) %>%
-  select(ctry_code)
-
-
-reg_tech %<>%
-  group_by(ctry_code, nace2_code) %>%
-  summarise(n_tech_reg = sum(field_weight)) %>%
-  ungroup() %>%
-  drop_na() 
+####
+rm(list=ls())
+reg_tech <- read.csv("Data/Data_reg_tech.csv", sep = ";", header = TRUE, dec=",")
 
 mat_reg_tech <- reg_tech %>%
   arrange(nace2_code, ctry_code) %>%
@@ -454,64 +300,51 @@ mat_reg_tech %>%
   arrange(desc(SE)) %>%
   head(10)
 
-#1.4.Nace By Country countries---- Skip this
-countries_geo <- Nace_all_patents %>%
-  distinct(ctry_code2, .keep_all = TRUE) %>%
-  select(ctry_code2)
+#calculate the g_tech_AI:
+library(janitor)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+matrix2 <- read.csv("Data/Matrix_Nace.csv", sep = ";", header = F)
+matrix2 <- matrix2 %>%
+  row_to_names(row_number = 1)
+matrix <- matrix2[,-1]
+rownames(matrix) <- matrix2[,1]
+matrix <- as.matrix(matrix)
+mat_tech_AI_Final <- matrix
 
-reg_tech <- Nace_all_patents %>%
-  group_by(appln_id) %>%
-  mutate(field_weight = 1 / n()) %>%
-  ungroup()
+mat_tech_rel_AI <- mat_tech_AI_Final %>% 
+  relatedness(method = "cosine")
 
-reg_tech %<>%
-  group_by(ctry_code2, nace2_code) %>%
-  summarise(n_tech_reg = sum(field_weight)) %>%
-  ungroup() %>%
-  drop_na() 
+nace2_names <- read.csv("Data/tls902_ipc_nace2.csv", sep = ";", header = TRUE)%>%
+  select(nace2_code, nace2_descr) %>%
+  distinct(nace2_code, .keep_all = TRUE) %>%
+  mutate(nace2_code = nace2_code) %>%
+  arrange(nace2_code)
 
-#library(tidyr)
-mat_reg_tech <- reg_tech %>%
-  arrange(nace2_code, ctry_code2) %>%
-  pivot_wider(names_from = nace2_code, values_from = n_tech_reg, values_fill = list(n_tech_reg = 0))
+g_tech_AI <- mat_tech_rel_AI %>% as_tbl_graph(directed = FALSE) %N>%
+  left_join(nace2_names %>% mutate(nace2_code = nace2_code %>% as.character()), by = c("name" = "nace2_code")) %>%
+  mutate(dgr = centrality_eigen(weights = weight)) %E>%
+  filter(weight >= mean(weight))
 
-rownames(mat_reg_tech) <- mat_reg_tech %>% pull(ctry_code2)
+coords_tech_AI <- g_tech_AI %>% igraph::layout.fruchterman.reingold() %>% as_tibble()
+colnames(coords_tech_AI) <- c("x", "y")
 
-mat_reg_tech %<>% select(-ctry_code2) %>%
-  as.matrix() %>%
-  round()
-mat_reg_tech[1:10, 1:10]
+#let's take a look at the most and less complex Nace fields:
+g_tech_AI %N>% 
+  arrange(desc(dgr)) %>%
+  as_tibble() %>%
+  slice(1:10)
 
-reg_RCA <- mat_reg_tech %>% location.quotient(binary = TRUE) %>% 
-  as.data.frame() %>% 
-  rownames_to_column("ctry_code2") %>% 
-  as_tibble() %>% 
-  gather(key = "nace2_code", value = "RCA", -ctry_code2) %>%
-  arrange(ctry_code2, nace2_code)
-reg_RCA %>% head()
-
-mat_reg_tech %>% 
-  Herfindahl() %>% 
-  as.data.frame() %>% 
-  rownames_to_column("ctry_code2") %>% 
-  rename(HH = ".") %>% 
-  arrange(desc(HH)) %>% 
-  head(10)
-
-mat_reg_tech %>% 
-  entropy() %>% 
-  as.data.frame() %>% 
-  rownames_to_column("ctry_code2") %>% 
-  rename(SE = ".") %>% 
-  arrange(desc(SE)) %>%
-  head(10)
+g_tech_AI %N>% 
+  arrange(dgr) %>%
+  as_tibble() %>%
+  slice(1:10)
 
 #1.5.Nace Visualization -----
 library(ggplot2)
 country_select <- c("CN", "US", "JP", "KR")
 i = 1
 Nace1 <- g_tech_AI %N>%
-  left_join(reg_RCA %>% filter(ctry_code2 == country_select[i]) %>% select(-ctry_code2), by = c("name" = "nace2_code")) %>%
+  left_join(reg_RCA %>% filter(ctry_code == country_select[i]) %>% select(-ctry_code), by = c("name" = "nace2_code")) %>%
   ggraph(layout = coords_tech_AI) + 
   geom_edge_link(aes(width = weight), alpha = 0.2, colour = "grey") + 
   geom_node_point(aes(colour = RCA, size = dgr)) + 
@@ -522,7 +355,7 @@ Nace1 <- g_tech_AI %N>%
 
 i = 2
 Nace2 <- g_tech_AI %N>%
-  left_join(reg_RCA %>% filter(ctry_code2 == country_select[i]) %>% select(-ctry_code2), by = c("name" = "nace2_code")) %>%
+  left_join(reg_RCA %>% filter(ctry_code == country_select[i]) %>% select(-ctry_code), by = c("name" = "nace2_code")) %>%
   ggraph(layout = coords_tech_AI) + 
   geom_edge_link(aes(width = weight), alpha = 0.2, colour = "grey") + 
   geom_node_point(aes(colour = RCA, size = dgr)) + 
@@ -533,7 +366,7 @@ Nace2 <- g_tech_AI %N>%
 
 i = 3
 Nace3 <- g_tech_AI %N>%
-  left_join(reg_RCA %>% filter(ctry_code2 == country_select[i]) %>% select(-ctry_code2), by = c("name" = "nace2_code")) %>%
+  left_join(reg_RCA %>% filter(ctry_code == country_select[i]) %>% select(-ctry_code), by = c("name" = "nace2_code")) %>%
   ggraph(layout = coords_tech_AI) + 
   geom_edge_link(aes(width = weight), alpha = 0.2, colour = "grey") + 
   geom_node_point(aes(colour = RCA, size = dgr)) + 
@@ -544,7 +377,7 @@ Nace3 <- g_tech_AI %N>%
 
 i = 4
 Nace4 <- g_tech_AI %N>%
-  left_join(reg_RCA %>% filter(ctry_code2 == country_select[i]) %>% select(-ctry_code2), by = c("name" = "nace2_code")) %>%
+  left_join(reg_RCA %>% filter(ctry_code == country_select[i]) %>% select(-ctry_code), by = c("name" = "nace2_code")) %>%
   ggraph(layout = coords_tech_AI) + 
   geom_edge_link(aes(width = weight), alpha = 0.2, colour = "grey") + 
   geom_node_point(aes(colour = RCA, size = dgr)) + 
@@ -553,8 +386,13 @@ Nace4 <- g_tech_AI %N>%
   theme_graph() +
   ggtitle("Technology Space: RCA Japan (Nace codes)")
 
-setwd("C:/Users/mathe/Google Drive/PhD/1.Paper 1 - Patent Analysis/0.Tudo escrito mais EMAEE e Workshop/GPT Nova vers?o P?s EMAEE/0.Novo draft/New Analysis Inventors Data/nace2 analysis")
+Nace1
+Nace2
+Nace3
+Nace4
 
+
+#For saving the pictures:
 jpeg("Nace_all_CN_persp_Period3.jpg", width = 14, height = 10, units = 'in', res = 200)
 Nace1
 dev.off()
