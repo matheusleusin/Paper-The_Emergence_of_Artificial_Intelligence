74568617f1d3e0ad42209f868410170e6f8b2d9f
matheusleusin <58182885+matheusleusin@users.noreply.github.com>
2020-07-06 18:43:49 +0200

    quot

--- Code_Technological_Space_Nace.R
+++ Code_Technological_Space_Nace.R
@@ -9,7 +9,7 @@ library(devtools)
 library(EconGeo) # Economic Geography functions
 
 library(ggplot2)
-library("data.table") #for reading the big files using fread and for replacing countries names (by AI_pat for example)
+library(data.table) #for reading the big files using fread and for replacing countries names (by AI_pat for example)
 
 #1.NACE data -----
 #1.1. Sparse matrix -----
